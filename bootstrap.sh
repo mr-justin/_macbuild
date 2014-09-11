4dfe00f465f9a6fa23836da53dca0b4cbc54f15b
Wes Kennedy <wes.kennedy@gmail.com>
2014-09-10 21:52:08 -0400

    Move XQuartz

--- bootstrap.sh
+++ bootstrap.sh
@@ -31,6 +31,16 @@ xcode-select --install
 
 sleep 90
 
+###     ###
+# XQuartz #
+###     ###
+
+cd /tmp
+wget http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.7.dmg
+hdiutil attach XQuartz-2.7.7.dmg
+sudo installer -pkg /Volumes/XQuartz-2.7.7/XQuartz.pkg -target /
+hdiutil unmount /Volumes/XQuartz-2.7.7
+
 ###            ###
 # Setup Homebrew #
 ###            ###
@@ -65,16 +75,6 @@ brew install wget
 ###              ###
 
 brew install git
-
-###     ###
-# XQuartz #
-###     ###
-
-cd /tmp
-wget http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.7.dmg
-hdiutil attach XQuartz-2.7.7.dmg
-sudo installer -pkg /Volumes/XQuartz-2.7.7/XQuartz.pkg -target /
-hdiutil unmount /Volumes/XQuartz-2.7.7
 
 ###              ###
 # Clone Repository #
